3b1256c0f8e2fab1feccdd12e3417fbf72219ed9
Joe Politz <joe.politz@gmail.com>
2023-03-22 15:54:19 -0700

    config and cleanup

--- src/main.rs
+++ src/main.rs
@@ -51,46 +51,6 @@ fn jit_expr(e : &Expr, ops : &mut dynasmrt::x64::Assembler) {
   }
 }
 
-fn libcall() {
-    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
-    let string = "Hello World!";
-
-    dynasm!(ops
-        ; .arch x64
-        ; ->hello:
-        ; .bytes string.as_bytes()
-    );
-
-    let hello = ops.offset();
-    dynasm!(ops
-        ; .arch x64
-        ; lea rcx, [->hello]
-        ; xor edx, edx
-        ; mov dl, BYTE string.len() as _
-        ; mov rax, QWORD print as _
-        ; sub rsp, BYTE 0x28
-        ; call rax
-        ; add rsp, BYTE 0x28
-        ; ret
-    );
-
-    println!("{:?}", ops);
-
-
-    let buf = ops.finalize().unwrap();
-
-    let hello_fn: extern "Rust" fn() -> bool = unsafe { mem::transmute(buf.ptr(hello)) };
-
-    assert!(hello_fn());
-}
-
-pub extern "Rust" fn print(buffer: *const u8, length: u64) -> bool {
-    io::stdout()
-        .write_all(unsafe { slice::from_raw_parts(buffer, length as usize) })
-        .is_ok()
-}
-
-
 fn compile_expr(e : &Expr, cmds : &mut Vec<String>) {
   match e {
     Expr::Num(n) => cmds.push(format!("mov rax, {n}")),
@@ -120,15 +80,11 @@ fn main() {
   let mut ops = dynasmrt::x64::Assembler::new().unwrap();
   let start = ops.offset();
 
-  libcall();
-
-/*
   jit_expr(&expr, &mut ops);
   dynasm!(ops; ret);
   let buf = ops.finalize().unwrap();
   let jitted_fn : extern fn() -> i32 = unsafe { mem::transmute(buf.ptr(start)) };
 
   println!("{}", jitted_fn());
-  */
  
 }
